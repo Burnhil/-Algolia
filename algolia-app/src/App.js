5548e0257f0802dcb43f3e370f5a2e1705f519be
Burnhil <Royal42Crown@gmail.com>
2020-06-23 21:30:33 -0500

    added author search

--- algolia-app/src/App.js
+++ algolia-app/src/App.js
@@ -4,12 +4,14 @@ import ResultPanel from './ResultPanel.js'
 
 class App extends Component {
 
+  //set up variables in constructor
   constructor(props){
     super(props);
 
     this.state= {
       results: [],
       value: null,
+      author: null,
       start: null,
       end: null
 
@@ -20,8 +22,7 @@ class App extends Component {
     //using this syntax, we no longer need to bind this method explicitly
     e.preventDefault();  
     //make use of the data collected in this form
-    //alert(this.state.value);
-    console.log("current search value = " + this.state.value);
+    //console.log("current search value = " + this.state.value);
     this.fetchData(this.state.value);
     this.setState({
       value: "",
@@ -36,8 +37,8 @@ class App extends Component {
     //using this syntax, we no longer need to bind this method explicitly
     e.preventDefault();  
     //make use of the data collected in this form
-    console.log("start date = " + this.state.start);
-    console.log("end date = " + this.state.end);
+    // console.log("start date = " + this.state.start);
+    // console.log("end date = " + this.state.end);
     this.fetchDateSearch(this.state.start, this.state.end);
     this.setState({
       start: "",
@@ -50,12 +51,28 @@ class App extends Component {
     );
   }
 
+  handleSubmit3 = (e) =>{
+    //using this syntax, we no longer need to bind this method explicitly
+    e.preventDefault();  
+    //make use of the data collected in this form
+    //alert(this.state.value);
+    // console.log("current search author = " + this.state.author);
+    this.fetchAuthor(this.state.author);
+    this.setState({
+      author: "",
+    },
+    () => {
+      console.log("this.state.author should be cleared. Its value is: " + this.state.author);
+    }
+    );
+  }
+
   handleChange = (e) =>{
     //update the state as this textbox's value is changed by the end user via typing
     this.setState({
       value: e.target.value,
     },
-    //remember to use the second argument for this.setState(), if you want to do something based on the update of state
+    //using console log to show current state from user input
     () =>{ 
       console.log("The state's value is now: " + this.state.value);
     });
@@ -64,10 +81,10 @@ class App extends Component {
   handleChange2 = (e) =>{
     //update the state as this textbox's value is changed by the end user via typing
     this.setState({
-      start: e.target.value,
-      
+      //handle change for start date value
+      start: e.target.value, 
     },
-    //remember to use the second argument for this.setState(), if you want to do something based on the update of state
+    //using console log to show current state from user input
     () =>{ 
       console.log("start = " + this.state.start);
       console.log("end = " + this.state.end);
@@ -76,25 +93,64 @@ class App extends Component {
 
   handleChange3 = (e) =>{
     //update the state as this textbox's value is changed by the end user via typing
-    this.setState({
-      
+    this.setState({ 
+      //handle change for end date value
       end: e.target.value,
     },
-    //remember to use the second argument for this.setState(), if you want to do something based on the update of state
+   //using console log to show current state from user input
     () =>{ 
       console.log("start = " + this.state.start);
       console.log("end = " + this.state.end);
     });
   }
 
+  handleChange4 = (e) =>{
+    //update the state as this textbox's value is changed by the end user via typing
+    this.setState({
+      author: e.target.value,
+    },
+    //using console log to show current state from user input
+    () =>{ 
+      console.log("The state's author is now: " + this.state.author);
+    });
+  }
+
+  fetchAuthor(input){
+    //use fetch funtion and callback to transform the data to the JSON structure
+    //console.log("author in state = " + input);
+
+    //setting up url with user input for author search
+    let firstPart = "http://hn.algolia.com/api/v1/search?tags=story,author_";
+    let fullSearch = firstPart + input;
+    //console.log(fullSearch);
+
+    //pass in url per user input
+    fetch(fullSearch)
+    .then(response => response.json())      //transform the text data to json which comes back as a promise use then() to continue
+    .then((results) => {                 // store json data in state
+      this.setState({
+        results: results.hits,
+      },
+    ()=> {
+      //for checking purposes, use optional second argument ot pass a funtion to see if state changed
+      console.log(`the authors ${this.state.results}`);
+      console.log(this.state.results[0].title);
+      //console.log("results length is " + this.state.theUsers.length);
+    });
+  });
+  }
+
   fetchData(input){
     //use fetch funtion and callback to transform the data to the JSON structure
-    console.log("input in state = " + input);
+    //console.log("input in state = " + input);
+
+    //setting up url with user input for search
     let firstPart = "https://hn.algolia.com/api/v1/search?query=";
     let secondPart = "&tags=story";
     let fullSearch = firstPart + input + secondPart;
-    console.log(fullSearch);
+    //console.log(fullSearch);
 
+    //pass in url per user input
     fetch(fullSearch)
     .then(response => response.json())      //transform the text data to json which comes back as a promise use then() to continue
     .then((results) => {                 // store json data in state
@@ -112,23 +168,26 @@ class App extends Component {
   }
 
   fetchDateSearch = (start,end) =>{
+    //console.log("we are in datasearch fetch with start = " + start + "end = " + end)
 
-    console.log("we are in datasearch fetch with start = " + start + "end = " + end)
-
+    //create function to get timestamp
     function toTimestamp(start){
-      var datum = Date.parse(start);
-      return datum/1000;
-   }
-   let inputStart = toTimestamp(start);
-   console.log(toTimestamp(start));
-   let inputEnd = toTimestamp(end);
-   console.log(toTimestamp(end));
+        var datum = Date.parse(start);
+        return datum/1000;
+    }
+    //create variables to store user input after conversion to timestamps
+    let inputStart = toTimestamp(start);
+    console.log(toTimestamp(start));
+    let inputEnd = toTimestamp(end);
+    console.log(toTimestamp(end));
 
+    //create url for search by date using timestamps
     let firstPart = "http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=created_at_i>";
     let secondPart = ",created_at_i<";
     let fullSearch = firstPart + inputStart + secondPart + inputEnd;
     console.log(fullSearch);
 
+    //pass in url per user input
    fetch(fullSearch)
     .then(response => response.json())      //transform the text data to json which comes back as a promise use then() to continue
     .then((results) => {                 // store json data in state
@@ -150,6 +209,8 @@ class App extends Component {
   render(){
 
     //console.log(this.state.results.length);
+
+    //create the results panels from the user input to be displayed
     let theResultsPanel = [];
     for(let i =0; i < this.state.results.length; i++){
       theResultsPanel.push(<div className="resultsPanel"><ResultPanel key={this.state.results[i]} result={this.state.results[i]}/> </div> )
@@ -158,14 +219,21 @@ class App extends Component {
     return (
       <div className="App">
 
-        {/** your form will want to go to a different page for submission, don't let it. */}
+        {/** create query button and textbox */}
         <form onSubmit={this.handleSubmit} className="formButton">
         {/** within the form element, you will include your various input elements */}
         <label>Query: <input value = {this.state.value} type="text" onChange={this.handleChange}/></label>
         <input type="submit" value="click to submit"/>  
         </form>
 
-        {/** your form will want to go to a different page for submission, don't let it. */}
+        {/** create author button and textbox */}
+        <form onSubmit={this.handleSubmit3} className="formButton">
+        {/** within the form element, you will include your various input elements */}
+        <label>Author Query: <input author = {this.state.author} type="text" onChange={this.handleChange4}/></label>
+        <input type="submit" value="click to submit"/>  
+        </form>
+
+        {/** create date buttons and textboxes */}
         <form onSubmit={this.handleSubmit2} className="formButton">
         {/** within the form element, you will include your various input elements */}
         <label>Start Date: <input start = {this.state.start} type="text" onChange={this.handleChange2} /></label>
@@ -173,6 +241,7 @@ class App extends Component {
         <input type="submit" value="click to submit"/>  
         </form>
 
+      {/** show the end results to the user */}
       {theResultsPanel}
 
       </div>
--- algolia-app/src/ResultPanel.js
+++ algolia-app/src/ResultPanel.js
@@ -2,15 +2,19 @@
 import React, { Component } from 'react';
 
 const ResultsList = (props) => {
+    //create the result list JSX to be returned 
     let resultListJSX = null
 
+    //check to see if props is null
     if(props.result !== null){
 
+    //create each JSX 
     let titleJSX = <li>{`Title: ${props.results.title}`}</li>;
     let authorJSX = <li>{`Author: ${props.results.author}`}</li>;
     let createdJSX = <li>{`Published Date: ${props.results.created_at}`}</li>;
     let urlJSX = <li>{`URL: ${props.results.url}`}</li>;
 
+    //combine JSX
     resultListJSX = <ul><header className="formButton">Article Results</header>
     <ul>{titleJSX}</ul>
     <ul>{authorJSX}</ul>
@@ -19,11 +23,13 @@ const ResultsList = (props) => {
     </ul>
     }
 
+    //return JSX
     return resultListJSX;
 }
 
 class ResultPanel extends Component {
 
+    //create constructor incase state is needed for each panel
     constructor(props){
         super(props);
     
@@ -31,7 +37,7 @@ class ResultPanel extends Component {
 
         };
       }
-
+      //return results back to app.js
     render(){
         console.log(this.props.result);
         return <div>
